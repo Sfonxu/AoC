d03364a7becd8ba73dcaf576163d69351261be1d
sfonxu <wronski.michal4@wp.pl>
2025-05-25 16:59:39 +0200

    remove build files

--- 2015/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/sfonxu/AoC/2015/day1/target/debug/day1: /home/sfonxu/AoC/2015/day1/src/main.rs
--- 2015/day2/target/debug/deps/day2-e89ab2d21f16a82a.d
+++
@@ -1,5 +0,0 @@
-/home/sfonxu/AoC/2015/day2/target/debug/deps/day2-e89ab2d21f16a82a: src/main.rs
-
-/home/sfonxu/AoC/2015/day2/target/debug/deps/day2-e89ab2d21f16a82a.d: src/main.rs
-
-src/main.rs:
